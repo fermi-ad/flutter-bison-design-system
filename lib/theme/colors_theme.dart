1e5b3bb74f375981c3f4b2839fb88c7f035cbc4b
Michael Guzman <mguzman@fnal.gov>
2026-01-12 19:03:47 -0600

    Added color palette and created light theme

---
+++ lib/theme/colors_theme.dart
@@ -0,0 +1,339 @@
+import 'package:flutter/material.dart';
+
+class ColorsTheme extends ThemeExtension<ColorsTheme> {
+  // Foundation Colors
+
+  // Beam Blue
+  static const _beamBlue0 = Color(0xFF000000);
+  static const _beamBlue10 = Color(0xFF0A1D3F);
+  static const _beamBlue20 = Color(0xFF113873);
+  static const _beamBlue30 = Color(0xFF1C55C4);
+  static const _beamBlue40 = Color(0xFF2D6AF0);
+  static const _beamBlue50 = Color(0xFF4C96F3);
+  static const _beamBlue60 = Color(0xFF65B0F6);
+  static const _beamBlue70 = Color(0xFF7DC4F8);
+  static const _beamBlue80 = Color(0xFFA0D6FB);
+  static const _beamBlue90 = Color(0xFFD3EAFF);
+  static const _beamBlue100 = Color(0xFFFFFFFF);
+  // Graphite Slate
+  static const _graphiteSlate0 = Color(0xFF000000);
+  static const _graphiteSlate10 = Color(0xFF1A1C22);
+  static const _graphiteSlate20 = Color(0xFF30343C);
+  static const _graphiteSlate30 = Color(0xFF474E59);
+  static const _graphiteSlate40 = Color(0xFF555F72);
+  static const _graphiteSlate50 = Color(0xFF707C91);
+  static const _graphiteSlate60 = Color(0xFF8E99AC);
+  static const _graphiteSlate70 = Color(0xFFAEB6C4);
+  static const _graphiteSlate80 = Color(0xFFCDD1DB);
+  static const _graphiteSlate90 = Color(0xFFE4E6EE);
+  static const _graphiteSlate100 = Color(0xFFFFFFFF);
+  // Pulse Red
+  static const _pulseRed0 = Color(0xFF000000);
+  static const _pulseRed10 = Color(0xFF220503);
+  static const _pulseRed20 = Color(0xFF50120D);
+  static const _pulseRed30 = Color(0xFFA11E1B);
+  static const _pulseRed40 = Color(0xFFD12828);
+  static const _pulseRed50 = Color(0xFFE45349);
+  static const _pulseRed60 = Color(0xFFF07B6E);
+  static const _pulseRed70 = Color(0xFFF6A49A);
+  static const _pulseRed80 = Color(0xFFFAD0CA);
+  static const _pulseRed90 = Color(0xFFFDEBE8);
+  static const _pulseRed100 = Color(0xFFFFFFFF);
+  // Synchro Green
+  static const _synchroGreen0 = Color(0xFF000000);
+  static const _synchroGreen10 = Color(0xFF001A00);
+  static const _synchroGreen20 = Color(0xFF003300);
+  static const _synchroGreen30 = Color(0xFF005C00);
+  static const _synchroGreen40 = Color(0xFF328C32);
+  static const _synchroGreen50 = Color(0xFF4FBC4F);
+  static const _synchroGreen60 = Color(0xFF52D75A);
+  static const _synchroGreen70 = Color(0xFF87EA87);
+  static const _synchroGreen80 = Color(0xFFBFF5BF);
+  static const _synchroGreen90 = Color(0xFFE5FBE5);
+  static const _synchroGreen100 = Color(0xFFFFFFFF);
+  // Vacuum Gray
+  static const _vacuumGray0 = Color(0xFF000000);
+  static const _vacuumGray10 = Color(0xFF1D1D1D);
+  static const _vacuumGray20 = Color(0xFF262626);
+  static const _vacuumGray30 = Color(0xFF484848);
+  static const _vacuumGray40 = Color(0xFF6d6d6d);
+  static const _vacuumGray50 = Color(0xFF969696);
+  static const _vacuumGray60 = Color(0xFFB6B6B6);
+  static const _vacuumGray70 = Color(0xFFCCCCCC);
+  static const _vacuumGray80 = Color(0xFFE9E9E9);
+  static const _vacuumGray90 = Color(0xFFF8F8F8);
+  static const _vacuumGray100 = Color(0xFFFFFFFF);
+  // Flux Orange
+  static const _fluxOrange0 = Color(0xFF000000);
+  static const _fluxOrange10 = Color(0xFF331800);
+  static const _fluxOrange20 = Color(0xFF662E00);
+  static const _fluxOrange30 = Color(0xFF993F00);
+  static const _fluxOrange40 = Color(0xFFCC5C00);
+  static const _fluxOrange50 = Color(0xFFE0672A);
+  static const _fluxOrange60 = Color(0xFFF87643);
+  static const _fluxOrange70 = Color(0xFFF48C5D);
+  static const _fluxOrange80 = Color(0xFFF9B38C);
+  static const _fluxOrange90 = Color(0xFFFBD9C1);
+  static const _fluxOrange100 = Color(0xFFFFFFFF);
+  // Burst Yellow
+  static const _burstYellow0 = Color(0xFF000000);
+  static const _burstYellow10 = Color(0xFF664D00);
+  static const _burstYellow20 = Color(0xFF997400);
+  static const _burstYellow30 = Color(0xFFCC9E00);
+  static const _burstYellow40 = Color(0xFFEEC200);
+  static const _burstYellow50 = Color(0xFFFBD800);
+  static const _burstYellow60 = Color(0xFFFFE600);
+  static const _burstYellow70 = Color(0xFFFFEB47);
+  static const _burstYellow80 = Color(0xFFFFF48F);
+  static const _burstYellow90 = Color(0xFFFFFCCD);
+  static const _burstYellow100 = Color(0xFFFFFFFF);
+  // Arc Purple
+  static const _arcPurple0 = Color(0xFF000000);
+  static const _arcPurple10 = Color(0xFF1D0033);
+  static const _arcPurple20 = Color(0xFF3C0091);
+  static const _arcPurple30 = Color(0xFF5C00C8);
+  static const _arcPurple40 = Color(0xFF8155E0);
+  static const _arcPurple50 = Color(0xFF9B8EF0);
+  static const _arcPurple60 = Color(0xFFA5B5F8);
+  static const _arcPurple70 = Color(0xFFBDC9FA);
+  static const _arcPurple80 = Color(0xFFD5DCFD);
+  static const _arcPurple90 = Color(0xFFECEEFD);
+  static const _arcPurple100 = Color(0xFFFFFFFF);
+  // Plasma Pink
+  static const _plasmaPink0 = Color(0xFF000000);
+  static const _plasmaPink10 = Color(0xFF4D004C);
+  static const _plasmaPink20 = Color(0xFF990080);
+  static const _plasmaPink30 = Color(0xFFCF35D1);
+  static const _plasmaPink40 = Color(0xFFDA4FC8);
+  static const _plasmaPink50 = Color(0xFFE96BBE);
+  static const _plasmaPink60 = Color(0xFFFF8AC4);
+  static const _plasmaPink70 = Color(0xFFFFADD6);
+  static const _plasmaPink80 = Color(0xFFFFCBE4);
+  static const _plasmaPink90 = Color(0xFFFFE6F4);
+  static const _plasmaPink100 = Color(0xFFFFFFFF);
+  // Ion Lime
+  static const _ionLime0 = Color(0xFF000000);
+  static const _ionLime10 = Color(0xFF263300);
+  static const _ionLime20 = Color(0xFF3F5800);
+  static const _ionLime30 = Color(0xFF5A8700);
+  static const _ionLime40 = Color(0xFF75B600);
+  static const _ionLime50 = Color(0xFFAAD802);
+  static const _ionLime60 = Color(0xFFB5D140);
+  static const _ionLime70 = Color(0xFFC9E362);
+  static const _ionLime80 = Color(0xFFDFF78E);
+  static const _ionLime90 = Color(0xFFF2FBBE);
+  static const _ionLime100 = Color(0xFFFFFFFF);
+  // Phton Cyan
+  static const _photonCyan0 = Color(0xFF000000);
+  static const _photonCyan10 = Color(0xFF002124);
+  static const _photonCyan20 = Color(0xFF00484E);
+  static const _photonCyan30 = Color(0xFF00A2A8);
+  static const _photonCyan40 = Color(0xFF05D7DD);
+  static const _photonCyan50 = Color(0xFF0AFAF2);
+  static const _photonCyan60 = Color(0xFF66FCF6);
+  static const _photonCyan70 = Color(0xFF99FDF8);
+  static const _photonCyan80 = Color(0xFFC4FEFA);
+  static const _photonCyan90 = Color(0xFFE3FEFC);
+  static const _photonCyan100 = Color(0xFFFFFFFF);
+
+  // Color Theme tokens
+
+  // Surface tokens
+  final Color surfaceDefault;
+  final Color surfaceHovered;
+  final Color surfacePressed;
+
+  // Text tokens
+  final Color textPlain;
+  final Color textPlaceholder;
+  final Color textInverse;
+  final Color textPrimary;
+  final Color textPrimaryHover;
+  final Color textSecondary;
+  final Color textError;
+  final Color textVisited;
+  final Color textDisabled;
+
+  final Color iconPlain;
+  final Color iconPlaceholder;
+  final Color inconInverse;
+  final Color iconPrimary;
+  final Color iconPrimaryHover;
+  final Color iconSecondary;
+  final Color iconError;
+  final Color iconSuccess;
+  final Color iconWarning;
+  final Color iconDisabled;
+
+  // Border tokens
+  final Color borderPlain;
+  final Color borderPrimary;
+  final Color borderSecondary;
+  final Color borderError;
+  final Color borderSuccess;
+  final Color borderWarning;
+  final Color borderDisabled;
+
+  // HSI stytle guide tokens
+  final Color dangerSignal;
+  final Color warningSignal;
+  final Color cautionSignal;
+  final Color userConfigurable;
+  final Color lineValueOutput;
+  final Color setpointInput;
+  final Color dynamicElement;
+  final Color historicalData;
+  final Color badData;
+  final Color delineateObjects;
+
+  // Plot/Graph tokens
+  final Color dataSet1;
+  final Color dataSet2;
+  final Color dataSet3;
+  final Color dataSet4;
+
+  // private constructor
+  const ColorsTheme._buildTheme({
+    required this.surfaceDefault,
+    required this.surfaceHovered,
+    required this.surfacePressed,
+    required this.textPlain,
+    required this.textPlaceholder,
+    required this.textInverse,
+    required this.textPrimary,
+    required this.textPrimaryHover,
+    required this.textSecondary,
+    required this.textError,
+    required this.textVisited,
+    required this.textDisabled,
+    required this.iconPlain,
+    required this.iconPlaceholder,
+    required this.inconInverse,
+    required this.iconPrimary,
+    required this.iconPrimaryHover,
+    required this.iconSecondary,
+    required this.iconError,
+    required this.iconSuccess,
+    required this.iconWarning,
+    required this.iconDisabled,
+    required this.borderPlain,
+    required this.borderPrimary,
+    required this.borderSecondary,
+    required this.borderError,
+    required this.borderSuccess,
+    required this.borderWarning,
+    required this.borderDisabled,
+    required this.dangerSignal,
+    required this.warningSignal,
+    required this.cautionSignal,
+    required this.userConfigurable,
+    required this.lineValueOutput,
+    required this.setpointInput,
+    required this.dynamicElement,
+    required this.historicalData,
+    required this.badData,
+    required this.delineateObjects,
+    required this.dataSet1,
+    required this.dataSet2,
+    required this.dataSet3,
+    required this.dataSet4,
+  });
+
+  // Factory for light mode:
+  factory ColorsTheme.light() {
+    return ColorsTheme._buildTheme(
+      surfaceDefault: _vacuumGray90,
+      surfaceHovered: _beamBlue90,
+      surfacePressed: _beamBlue80,
+      textPlain: _vacuumGray10,
+      textPlaceholder: _vacuumGray30,
+      textInverse: _vacuumGray90,
+      textPrimary: _beamBlue40,
+      textPrimaryHover: _beamBlue30,
+      textSecondary: _graphiteSlate40,
+      textError: _pulseRed40,
+      textVisited: _arcPurple60,
+      textDisabled: _vacuumGray50,
+      iconPlain: _vacuumGray10,
+      iconPlaceholder: _vacuumGray30,
+      inconInverse: _vacuumGray90,
+      iconPrimary: _beamBlue40,
+      iconPrimaryHover: _beamBlue30,
+      iconSecondary: _graphiteSlate40,
+      iconError: _pulseRed40,
+      iconSuccess: _synchroGreen30,
+      iconWarning: _fluxOrange60,
+      iconDisabled: _vacuumGray50,
+      borderPlain: _vacuumGray60,
+      borderPrimary: _beamBlue40,
+      borderSecondary: _graphiteSlate40,
+      borderError: _pulseRed40,
+      borderSuccess: _synchroGreen30,
+      borderWarning: _fluxOrange60,
+      borderDisabled: _vacuumGray60,
+      dangerSignal: _pulseRed40,
+      warningSignal: _fluxOrange60,
+      cautionSignal: _burstYellow60,
+      userConfigurable: _photonCyan50,
+      lineValueOutput: _synchroGreen50,
+      setpointInput: _beamBlue40,
+      dynamicElement: _beamBlue40,
+      historicalData: _arcPurple60,
+      badData: _plasmaPink30,
+      delineateObjects: _vacuumGray100,
+      dataSet1: _ionLime50,
+      dataSet2: _burstYellow40,
+      dataSet3: _fluxOrange70,
+      dataSet4: _plasmaPink60,
+    );
+  }
+
+  // factory for dark mode
+  factory ColorsTheme.dark() {
+    return ColorsTheme._buildTheme(
+      surfaceDefault: _plasmaPink90,
+      surfaceHovered: _plasmaPink90,
+      surfacePressed: _plasmaPink90,
+      textPlain: _plasmaPink90,
+      textPlaceholder: _plasmaPink90,
+      textInverse: _plasmaPink90,
+      textPrimary: _plasmaPink90,
+      textPrimaryHover: _plasmaPink90,
+      textSecondary: _plasmaPink90,
+      textError: _plasmaPink90,
+      textVisited: _plasmaPink90,
+      textDisabled: _plasmaPink90,
+      iconPlain: _plasmaPink90,
+      iconPlaceholder: _plasmaPink90,
+      inconInverse: _plasmaPink90,
+      iconPrimary: _plasmaPink90,
+      iconPrimaryHover: _plasmaPink90,
+      iconSecondary: _plasmaPink90,
+      iconError: _plasmaPink90,
+      iconSuccess: _plasmaPink90,
+      iconWarning: _plasmaPink90,
+      iconDisabled: _plasmaPink90,
+      borderPlain: _plasmaPink90,
+      borderPrimary: _plasmaPink90,
+      borderSecondary: _plasmaPink90,
+      borderError: _plasmaPink90,
+      borderSuccess: _plasmaPink90,
+      borderWarning: _plasmaPink90,
+      borderDisabled: _plasmaPink90,
+      dangerSignal: _plasmaPink90,
+      warningSignal: _plasmaPink90,
+      cautionSignal: _plasmaPink90,
+      userConfigurable: _plasmaPink90,
+      lineValueOutput: _plasmaPink90,
+      setpointInput: _plasmaPink90,
+      dynamicElement: _plasmaPink90,
+      historicalData: _plasmaPink90,
+      badData: _plasmaPink90,
+      delineateObjects: _plasmaPink90,
+      dataSet1: _plasmaPink90,
+      dataSet2: _plasmaPink90,
+      dataSet3: _plasmaPink90,
+      dataSet4: _plasmaPink90,
+    );
+  }
+}
