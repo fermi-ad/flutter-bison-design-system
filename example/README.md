3b23cad967ce6af6c9a024e2ac1867db67e765a2
Connor Howington <connor.howington@gmail.com>
2026-02-16 12:56:38 -0600

    Fix failing integration workflow (#4)
    * Update workflow commit hash
    
    * Grant write permissions to integration workflow
    
    * Exclude generated files
    
    * Replace print with debugPrint
    
    * Commit generated directories file
    
    * Update integration workflow commit hash

--- example/README.md
+++ example/README.md
@@ -4,14 +4,14 @@ A Flutter Widgetbook catalog to view and test design system components.
 
 ## Getting Started
 
-Generate the directories files:
-
-```sh
-dart run build_runner build -d
-```
-
 Start up the Widgetbook:
 
 ```sh
 flutter run -d web-server
+```
+
+If you add new components or use cases, run this to regenerate the directories files:
+
+```sh
+dart run build_runner build -d
 ```
---
+++ example/lib/main.directories.g.dart
@@ -0,0 +1,37 @@
+// dart format width=80
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AppGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:design_system_catalog/button.dart'
+    as _design_system_catalog_button;
+import 'package:widgetbook/widgetbook.dart' as _widgetbook;
+
+final directories = <_widgetbook.WidgetbookNode>[
+  _widgetbook.WidgetbookFolder(
+    name: 'core_widgets',
+    children: [
+      _widgetbook.WidgetbookFolder(
+        name: 'buttons',
+        children: [
+          _widgetbook.WidgetbookComponent(
+            name: 'Button',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _design_system_catalog_button.buildCoolButtonUseCase,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  ),
+];
