ba38e9f85891ba7919b618ae771007014ef9416f
Michael Guzman <mguzman@fnal.gov>
2026-03-11 11:43:56 -0500

    removed unused shown member

--- lib/core_widgets.dart
+++ lib/core_widgets.dart
@@ -1,5 +1,4 @@
-export 'src/core_widgets/buttons/bison_button.dart'
-    show BisonButton, BisonButtonType;
+export 'src/core_widgets/buttons/bison_button.dart' show BisonButton;
 export 'src/core_widgets/buttons/bison_icon_button.dart' show BisonIconButton;
 export 'src/core_widgets/menus/bison_menu.dart'
     show BisonMenu, BisonMenuItem, BisonMenuTriggerAction;
