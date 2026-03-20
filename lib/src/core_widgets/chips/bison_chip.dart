f68811e123a2ef88ba1fb8203c32c85bd8edbdb8
Michael Guzman <mguzman@fnal.gov>
2026-03-20 11:47:22 -0500

    initial creation of bisonChip

---
+++ lib/src/core_widgets/chips/bison_chip.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class BisonChip extends StatelessWidget {
+  final String label;
+  final VoidCallback? onLeftPressed;
+  final VoidCallback? onRightPressed;
+
+  const BisonChip._({
+    required this.label,
+    this.onLeftPressed,
+    this.onRightPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
